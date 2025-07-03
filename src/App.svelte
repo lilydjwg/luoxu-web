26c53f01ea698663ea14385d54bef43e558ca21a
lilydjwg <lilydjwg@gmail.com>
2025-07-03 23:27:58 +0800

    update LUOXU_URL

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
   import { sleep } from "./util.js";
   import "./global.css";
 
-  const LUOXU_URL = "https://lab.lilydjwg.me/luoxu";
+  const LUOXU_URL = "https://apps.archlinuxcn.org/luoxu";
   const islocal = LUOXU_URL.startsWith("http://localhost");
   let groups: { group_id: string; name: string }[] = $state([]);
   let group: string = $state();
